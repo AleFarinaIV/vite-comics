561c64a7a9e16b4c2d8ae1ca04e9d79ded5f8543
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-29 15:41:18 +0200

    [UPD] footer component: first part done

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,9 +5,76 @@ export default {
 </script>
 
 <template>
-<h1>Questo è il Footer</h1>
+    <div class="bg_footer text-white p-4">
+        <div class="container p-4">
+            <div class="row">
+                <div class="col-4">
+                    <div class="row">
+                        <div class="col-4 p-0">
+                            <h6>DC COMICS</h6>
+                            <ul class="list-unstyled">
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                            <h6>SHOP</h6>
+                            <ul class="list-unstyled">
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                        </div>
+                        <div class="col-4 p-0">
+                            <h6>DC</h6>
+                            <ul class="list-unstyled">
+                                <li>Thermes Of Use</li>
+                                <li>Privacy policy (New)</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </div>
+                        <div class="col-4 p-0">
+                            <h6>SITES</h6>
+                            <ul class="list-unstyled">
+                                <li>DC</li>
+                                <li>MAD MAgazine</li>
+                                <li>MDC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    .bg_footer {
+        background-image: url(../assets/img/footer-bg.jpg);
+        background-size: cover;
+    }
 
+    .bg_grey {
+        background-color: #303030;
+    }
+
+    ul {
+        margin-top: 10px;
+        li {
+            font-size: 12px;
+            color: grey;
+        }
+    }
 </style>
