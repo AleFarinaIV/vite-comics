0eafab7c4bdf87d496b81c38120a01cc510fc946
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-29 13:46:52 +0200

    [UPD] init components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+<h1>Questo è il Footer</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+<h1>Questo è l'Header!</h1>
+</template>
+
+<style scoped>
+
+</style>
