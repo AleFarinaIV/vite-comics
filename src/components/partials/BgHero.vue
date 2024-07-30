9b038dea0fd7323043887909c67881204b3dd639
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-30 17:55:09 +0200

    [UPD] created component for hero bg

---
+++ src/components/partials/BgHero.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+    
+    }
+</script>
+
+<template>
+
+    <div class="hero_bg"></div>
+
+</template>
+
+<style scoped>
+
+    .hero_bg {
+        background-image: url(../../assets/img/jumbotron.jpg);
+        background-size: cover;
+        min-height: 310px;
+    }
+
+</style>
